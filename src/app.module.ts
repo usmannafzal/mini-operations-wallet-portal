18f34e3004d857986abbf649fff776545b679577
Usman Afzal <usman@Usmans-MacBook-Pro.local>
2026-08-11 00:32:35 +0500

    Configs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST') ?? 'localhost',
+        port: configService.get<number>('DB_PORT') ?? 5432,
+        username: configService.get<string>('DB_USERNAME') ?? 'postgres',
+        password: configService.get<string>('DB_PASSWORD') ?? 'postgres',
+        database: configService.get<string>('DB_NAME') ?? 'wallet',
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
